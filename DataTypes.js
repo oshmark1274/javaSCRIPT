91945d8eca5ebece3a31b2f4222f4fd14cfeedf8
osatehinse sunday <oshmark1274@gmail.com>
2024-06-18 05:09:20 -0700

    mark

---
+++ DataTypes.js
@@ -0,0 +1,55 @@
+// Primitive  : string ; number ; int ; null ; bigit ; boolean ; umdefined 
+// Non- Primitive : object (object ; array )
+
+// var ; let const
+
+//variable
+var first = "welcome"
+// var = keyword; first = name/declaration ; "welcome" : string = variable value
+var first = "good"
+// console.log(first)
+
+// number = 000
+let dollars = 1500
+dollars = 1405
+dollars = 1320
+let dolars2 = dollars
+
+// boolean = true/false
+const light = true
+const light2 = light
+const greet = "Good morning"
+console.log(greet)
+
+// null
+const greet2 = null
+console.log(greet2)
+
+
+
+// object : object & array
+
+// object = (key : value)
+const boy = {"maths": 20}
+
+// json data []
+const jss1 = [
+     {"Bola" : "female"},
+     {"kola" : "male"},
+     {"jane" : "female"},
+
+]
+
+console.log(jss1)
+
+// array
+const allData = [20, "boy", {"age" : 15}, undefined, null, [20, "boy", {"age" : 15}, undefined, null]]
+
+const facebookusers = [{
+    " name" : "baller skibby",
+    "location" : "California, ibadan",
+    "Age" : 18,
+    "email" : "baller@test.com"
+},
+]
+   console.log(facebookusers)
---
+++ stringMethod.js
@@ -0,0 +1,22 @@
+// length
+const greet = "good morning"
+const greet1 = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates, doloremque a ab placeat blanditiis inventore non praesentium asperiores illo voluptas ipsum molestias animi cumque excepturi vero velit dolores totam quae!"
+// console.log(greet1.length)
+
+// indexOf()
+const greet2 = "good morning"
+// const greet3 = greet2.indexOf("m")
+// console.log(greet2.indexOf("n", 9))
+
+// toUpperCase() & toLowerCase()
+const greet4 = "good morning"
+const greet5 = greet4.toUpperCase()
+const greet6 = greet5.toLowerCase()
+// console.log(greet5)
+// console.log(greet6)
+
+// concat()
+const greet7 = "Hello world. "
+const greet8 = greet7.concat("How are you? ", "Have a great day. ", "Please,call me")
+// const greet9 = greet8.concat()
+console.log(greet8)
